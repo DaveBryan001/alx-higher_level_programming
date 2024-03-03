5496579cae5585eb0f67127dee11206180810a4f
Dave Bryan <dave.tamakloe@gmail.com>
2024-03-03 11:20:55 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,12 +4,12 @@
 
 class Square():
     """ this class defines a square"""
-    
+
     def __init__(self, size):
         """ initialize the square with the given size.
 
         args:
             size(int): size of the square
-        
+
         """
         self.__size = size
