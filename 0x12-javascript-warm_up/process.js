1a97f8a8ee4fd5653d970528186f2e54731c4f6e
davebryan001 <dave.tamakloe@gmail.com>
2024-06-20 01:38:30 +0000

    task 1

--- 0x12-javascript-warm_up/process.js
+++ 0x12-javascript-warm_up/process.js
@@ -1,6 +1,14 @@
-const { argv } = require('node:process');
+/* const { argv } = require('node:process');
 
 // print process.argv
 argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
 });
+*/
+
+function myFunction(a) {
+  console.log(a);
+}
+
+const a = 12;
+myFunction(89);
