247eabe04b00d66ad14b412ecd1d1990c9765197
davebryan001 <dave.tamakloe@gmail.com>
2024-06-20 01:53:26 +0000

    fix errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (this._isValid(w) && this._isValid(h)) {
-      this.width = w;
-      this.height = h;
+    constructor(w, h) {
+        if (this._isValid(w) && this._isValid(h)) {
+            this.width = w;
+            this.height = h;
+        }
     }
-  }
 
-  _isValid (value) {
-    return Number.isInteger(value) && value > 0;
-  }
+    _isValid(value) {
+        return Number.isInteger(value) && value > 0;
+    }
 }
 
-export default Rectangle;
+module.exports = Rectangle;
