77f54de2294a6ff6cae2364da78c8ee13818e1d6
davebryan001 <dave.tamakloe@gmail.com>
2024-06-20 03:16:27 +0000

    fix checker errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+/*
 const fs = require('fs');
 
 const [, , fileA, fileB, fileC] = process.argv;
@@ -24,3 +25,34 @@ function concatenateFiles (fileA, fileB, fileC) {
 }
 
 concatenateFiles(fileA, fileB, fileC);
+*/
+
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf-8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading file ${sourceFile1}:`, err);
+    return;
+  }
+
+  fs.readFile(sourceFile2, 'utf-8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading file ${sourceFile2}:`, err);
+      return;
+    }
+
+    const combinedData = data1 + data2;
+
+    fs.writeFile(destinationFile, combinedData, (err) => {
+      if (err) {
+        console.error(`Error writing to file ${destinationFile}:`, err);
+        return;
+      }
+      console.log(`Files concatenated successfully to ${destinationFile}`);
+    });
+  });
+});
